3e39b718763a556bab3ccf0f4f788737c8d6d253
Max Robson <maxwell.robson@gmail.com>
2017-11-24 14:36:05 +0000

    com 4, excercise 5

--- pet_shop_MR.rb
+++ pet_shop_MR.rb
@@ -9,3 +9,11 @@ end
 def add_or_remove_cash(pet_shop, value)
   pet_shop[:admin][:total_cash] = total_cash(pet_shop) + value
 end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, sold)
+  pet_shop[:admin][:pets_sold] = pets_sold(pet_shop) + sold
+end
